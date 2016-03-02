257bc3f788c8fd302df5d5e5d065f4c0b7926c64
Cody Bouscaren <codybousc@gmail.com>
2016-03-02 15:55:11 -0500

    Youtube api key & basic search bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+//YouTube API Key
+const API_KEY = 'AIzaSyCjXuQOPTrrQsCRG9_13GPaBjXqpvWcm5Y';
+
+
 //Create a new component that should produce some HTML
 const App = () => {
-  return <div> Hello! </div>
+  return (
+    <div>
+    <SearchBar />
 
+    </div>
+  )
 }
 
 
