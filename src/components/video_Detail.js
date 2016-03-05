ca39a015c7d74b64389e1e81f1caf5f714dceaeb
Cody Bouscaren <codybousc@gmail.com>
2016-03-05 16:27:26 -0500

    Correctly displays Video

--- src/components/video_Detail.js
+++ src/components/video_Detail.js
@@ -6,11 +6,12 @@ const VideoDetail = ({video}) => {
   }
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
-  console.log("url = ", url); 
+  console.log("video = ", video);
+  console.log("url = ", url);
 
    return (
      <div className="video-detail col-md-8">
-        <div className="embed-responsive embed-responsive16by9">
+        <div className="embed-responsive embed-responsive-16by9">
             <iframe className="embed-responsive-item" src={url}></iframe>
         </div>
         <div className="details">
