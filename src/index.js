f39f39524be62ff59c3012746645b07b6446858e
Cody Bouscaren <codybousc@gmail.com>
2016-03-05 17:10:43 -0500

    Updates video when sidebar videos are selected

--- src/index.js
+++ src/index.js
@@ -15,18 +15,25 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfer girls'}, (data) => {
-      this.setState({videos: data});
+      this.setState({videos: data,
+      selectedVideo: data[0]
+      });
     });
   }
   render () {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+         videos={this.state.videos} />
       </div>
     )
   }
